0b67d4d195b7948c379b603b5d1e9b68238961d1
Luis Victoria <luisvictoria@protonmail.com>
2020-12-07 23:10:28 -0500

    Solve 2020/1 (AdventOfCode)

---
+++ AdventOfCode/2020/1-1.py
@@ -0,0 +1,15 @@
+file = open('1.txt', 'r')
+list = []
+
+for line in file:
+	list.append(line.strip())
+
+def search():
+	for i in range(len(list)-1):
+		for j in range(i+1,len(list)):
+			if(int(list[i]) + int(list[j]) == 2020):
+				print('i: ' + str(list[i]) + ', j: ' + str(list[j]))
+				print('product: ' + str((int(list[i]) * int(list[j]))))
+				return
+
+search()
---
+++ AdventOfCode/2020/1-2.py
@@ -0,0 +1,22 @@
+file = open('1.txt', 'r')
+list = []
+
+for line in file:
+	list.append(int(line.strip()))
+
+list.sort()
+
+def search():
+	for i in range(len(list)-2):
+		for j in range(i+1, len(list)-1):
+			for k in reversed(range(len(list))):
+				if(list[i]+list[j]+list[k] == 2020):
+					print('i: ' + str(list[i]) + ', j: ' + str(list[j]) + ', k: ' + str(list[k]))
+					print('product: ' + str(list[i]*list[j]*list[k]))
+					return
+				elif(list[i]+list[j]+list[k] < 2020):
+					break # all searches past this point are redundant
+				else:
+					continue
+
+search()
