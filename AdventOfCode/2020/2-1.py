e283eba0fa545d6ab6ec85477dc053942b5f5273
Luis Victoria <luisvictoria@protonmail.com>
2020-12-08 00:06:14 -0500

    Solve 2020/2 (AdventOfCode)

---
+++ AdventOfCode/2020/2-1.py
@@ -0,0 +1,26 @@
+file = open('2.txt', 'r')
+list = []
+
+for line in file:
+	list.append(line.strip())
+
+def valid(lower, upper, char, string):
+	count = 0
+	for c in string:
+		if(c == char):
+			count += 1
+	return count>=lower and count<=upper
+
+def search():
+	count = 0
+	for i in range(len(list)):
+		lower = list[i].split('-')[0]
+		upper = (list[i].split('-')[1]).split(' ')[0]
+		char = (list[i].split(' ')[1]).split(':')[0]
+		string = list[i].split(' ')[2]
+		if(valid(int(lower), int(upper), str(char), str(string))):
+			count += 1
+	
+	print('total valid passwords: ' + str(count))
+
+search()
