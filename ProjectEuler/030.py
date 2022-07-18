e0dc149256a9f3d1868ce0638bd2ea86327b7302
Luis Victoria <x@lambda.lv>
2022-07-18 02:54:26 -0700

    Solve 030 (ProjectEuler)

---
+++ ProjectEuler/030.py
@@ -0,0 +1,15 @@
+power = 5
+
+def digit_sum(num):
+	sum = 0
+	str_num = str(num)
+	for c in str_num:
+		sum += int(c) ** power
+	return sum
+
+sum_nums = 0
+for i in range(2,999999):
+	if i == digit_sum(i):
+		sum_nums += i
+
+print(sum_nums)
