58b1b829664e02b0ddc8db7aea0245e4488d68dd
Luis Victoria <x@lambda.lv>
2023-09-25 02:14:46 -0400

    Solve 4A

---
+++ CodeForces/4A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <bitset>
+
+int main() {
+    // Read input
+    int n;
+    std::cin >> n;
+
+    // Extract final bit; must be divisible by two
+    int finalBit = n & 0b1;
+
+    if(n!=2 && finalBit==0b0) {
+        std::cout << "YES\n";
+    } else {
+        std::cout << "NO\n";
+    }
+
+    return 0;
+}
