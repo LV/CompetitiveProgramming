eec1860e0aed72258dab2a693b2b4565108e44b1
Luis Victoria <x@lambda.lv>
2023-10-24 23:45:33 -0400

    Solve AoC 2 part 1

---
+++ AdventOfCode/2015/02-1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+import pathlib
+
+file_path: pathlib.Path = pathlib.Path(__file__).parent.resolve() / "02.txt"
+
+
+@dataclass
+class Dimension:
+    l: int
+    w: int
+    h: int
+
+
+def surface_area(d: Dimension) -> int:
+    return (2 * d.l * d.w) + (2 * d.w * d.h) + (2 * d.h * d.l)
+
+
+def smallest_area(d: Dimension) -> int:
+    return min([d.l * d.w, d.w * d.h, d.h * d.l])
+
+
+def parse_line(line: str) -> Dimension:
+    tupl = line.split("x")
+    return Dimension(l=int(tupl[0]), w=int(tupl[1]), h=int(tupl[2]))
+
+
+def main() -> None:
+    total_needed: int = 0
+
+    with open(file_path, "r") as f:
+        for line in f:
+            line: str = line.strip()  # remove newline character at the end
+
+            d: Dimension = parse_line(line)
+            print(d)
+
+            total_needed += surface_area(d)
+            total_needed += smallest_area(d)
+
+    print(total_needed)
+
+
+if __name__ == "__main__":
+    main()
