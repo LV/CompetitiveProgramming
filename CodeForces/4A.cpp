2bd924caf926ebb4d697249d9fe26c71f4429d5e
Luis Victoria <x@lambda.lv>
2024-02-22 03:30:46 -0500

    Convert 4A.cpp to one-liner

--- CodeForces/4A.cpp
+++ CodeForces/4A.cpp
@@ -9,11 +9,8 @@ int main() {
     // Extract final bit; must be divisible by two
     int finalBit = n & 0b1;
 
-    if(n!=2 && finalBit==0b0) {
-        std::cout << "YES\n";
-    } else {
-        std::cout << "NO\n";
-    }
+    if(n!=2 && finalBit==0b0) std::cout << "YES\n";
+    else std::cout << "NO\n";
 
     return 0;
 }
