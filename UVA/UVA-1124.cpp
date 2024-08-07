825228e97a7da351b6cb9a1281cf1254e6b179f2
Luis Victoria <v@lambda.lv>
2024-08-06 22:33:27 -0400

    Solve Celebrity Jeopardy

---
+++ UVA/UVA-1124.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file UVA-1124.cpp
+ * @brief Celebrity Jeopardy
+ * @note https://vjudge.net/problem/UVA-1124
+ * @author Luis Victoria
+ * @date 2024-08-06 22:28:35
+ */
+
+#include <iostream>
+#include <string>
+
+void solve()
+{
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        for (char &ch : line)
+            std::cout << ch;
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    solve();
+
+    return 0;
+}
+
