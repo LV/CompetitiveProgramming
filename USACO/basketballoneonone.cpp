f1e110350f1620e57811cac7c819e2cfe50b5ad3
Luis Victoria <v@lambda.lv>
2024-09-29 03:37:56 -0400

    Solve `basketballoneonone`

---
+++ USACO/basketballoneonone.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <string>
+
+int main() {
+    std::ios::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::string inp;
+    std::cin >> inp;
+
+    std::cout << inp[inp.size()-2];
+}
